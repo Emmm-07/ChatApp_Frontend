5f1f528d87b67299f23d32ba02cf94f463ee152d
Emmm-07 <elemem23@gmail.com>
2025-03-04 17:34:06 +0800

    fix: use production urls

--- config.jsx
+++ config.jsx
@@ -1,16 +1,16 @@
 //This is to declare global variables
 
 //for testing http://
-export const hostUrl = 'http://127.0.0.1:8000'
+// export const hostUrl = 'http://127.0.0.1:8000'
 //for deployment https://
-// export const hostUrl = 'https://em-chat-server.onrender.com'
+export const hostUrl = 'https://em-chat-server.onrender.com'
 
 
 // for WebSocket:
 // Testing: ws://
-export const wsURL = 'ws://127.0.0.1:8000/ws/socketserver'
+// export const wsURL = 'ws://127.0.0.1:8000/ws/socketserver'
 //Deployment: wss://
-// export const wsURL = 'wss://em-chat-server.onrender.com/ws/socketserver'
+export const wsURL = 'wss://em-chat-server.onrender.com/ws/socketserver'
 
 
 
